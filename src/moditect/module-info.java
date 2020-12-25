db103f10a54027ae285498affeb0a9bb6c948157
Rob Spoor <rob@icemanx.nl>
2020-12-25 15:18:05 +0100

    Marked java.xml dependency as transitive, as QName used in the API

--- src/moditect/module-info.java
+++ src/moditect/module-info.java
@@ -1,6 +1,6 @@
 module com.github.robtimus.obfuscation.xml {
     requires transitive com.github.robtimus.obfuscation;
-    requires java.xml;
+    requires transitive java.xml;
     requires com.ctc.wstx;
     requires org.slf4j;
 
